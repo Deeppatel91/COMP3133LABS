4a9a15bf61ec256bcb67dca6d4fe7344f2a02578
Deeppatel91 <deeppatel9171@gmail.com>
2025-03-23 17:42:36 -0400

    added wk08 labs

---
+++ Lab-8/lab-08/src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero'; // Ensure Hero interface is defined
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Spider-Man' },
+  { id: 2, name: 'Iron-Man' },
+  { id: 3, name: 'Captain-America' },
+  { id: 4, name: 'Black-Widow' },
+  { id: 5, name: 'Thor' }
+];
